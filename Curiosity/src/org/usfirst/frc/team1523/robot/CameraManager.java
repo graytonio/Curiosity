a6875a1c8e969e30cdbc8a5348a562ad8a18ddb0
graytonio <Grayton@Graytons-MacBook-Pro.local>
2017-03-28 06:40:06 -0400

    Fixed Grip Issues
    Fixed issue where VisionThread would not run

--- Curiosity/src/org/usfirst/frc/team1523/robot/CameraManager.java
+++ Curiosity/src/org/usfirst/frc/team1523/robot/CameraManager.java
@@ -5,12 +5,12 @@ import edu.wpi.first.wpilibj.CameraServer;
 
 public class CameraManager {
 	
-	public static UsbCamera cam1;
+//	public static UsbCamera cam1;
 	public static UsbCamera cam2;
 
 	public static void init(){
 		//Grab Camera Feeds
-		cam1 = CameraServer.getInstance().startAutomaticCapture(0);
+//		cam1 = CameraServer.getInstance().startAutomaticCapture(0);
 		cam2 = CameraServer.getInstance().startAutomaticCapture(1);
 	}
 	
@@ -18,7 +18,7 @@ public class CameraManager {
 	 * Teleop Camera Settings
 	 */
 	public static void tele(){
-		cam1.setExposureManual(30);
+//		cam1.setExposureManual(30);
 		cam2.setExposureManual(30);
 	}
 	
@@ -26,7 +26,7 @@ public class CameraManager {
 	 * Autonomous Camera Settings
 	 */
 	public static void auto(){
-		cam1.setExposureManual(-50);
+//		cam1.setExposureManual(-50);
 		cam2.setExposureManual(-50);
 	}
 }
--- Curiosity/src/org/usfirst/frc/team1523/robot/Robot.java
+++ Curiosity/src/org/usfirst/frc/team1523/robot/Robot.java
@@ -9,8 +9,10 @@ import org.usfirst.frc.team1523.robot.subsystems.Ball;
 import org.usfirst.frc.team1523.robot.subsystems.Drive;
 import org.usfirst.frc.team1523.robot.subsystems.Gear;
 import org.usfirst.frc.team1523.robot.subsystems.Rope;
+import org.usfirst.frc.team1523.vision.GripPipeline;
 
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Command;
@@ -69,13 +71,18 @@ public class Robot extends IterativeRobot {
 		SmartDashboard.putData("Auto Choice", chooser);
 		
 		//Start Vision Thread
-		vision = new VisionThread(CameraManager.cam1, new GripPipeline(), grip ->{
-			Rect r1 = Imgproc.boundingRect(grip.convexHullsOutput().get(0));
-			Rect r2 = Imgproc.boundingRect(grip.convexHullsOutput().get(1));
-			x = ((r1.x + r1.width/2) + (r2.x + r2.width/2)) / 2;
-			distance = r1.x;
-			System.out.println("STEP: X: " + x + " Distance: " + distance);
+		vision = new VisionThread(CameraServer.getInstance().startAutomaticCapture(0), new GripPipeline(), grip ->{
+			if(grip.convexHullsOutput().size()==2){
+				Rect r1 = Imgproc.boundingRect(grip.convexHullsOutput().get(0));
+				Rect r2 = Imgproc.boundingRect(grip.convexHullsOutput().get(1));
+				x = ((r1.x + r1.width/2) + (r2.x + r2.width/2)) / 2;
+				distance = r1.x;
+				System.out.println("STEP: X: " + x + " Distance: " + distance);
+			}else{
+				System.out.println("NO TARGET FOUND" + grip.convexHullsOutput().size());
+			}
 		});
+		
 		vision.start();
 	}
 
@@ -88,6 +95,7 @@ public class Robot extends IterativeRobot {
 	@Override
 	public void disabledPeriodic() {
 		Scheduler.getInstance().run();
+//		System.out.println("X: " + x);
 		log();
 	}
 
